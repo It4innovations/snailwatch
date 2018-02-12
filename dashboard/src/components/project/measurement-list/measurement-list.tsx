e65b568a2de00da5110d07551c4e7be4799918f9
Jakub Beránek <berykubik@gmail.com>
2018-02-12 22:51:03 +0100

    ENH: optimize measurement list some more

--- dashboard/src/components/project/measurement-list/measurement-list.tsx
+++ dashboard/src/components/project/measurement-list/measurement-list.tsx
@@ -2,8 +2,9 @@ import React, {PureComponent} from 'react';
 import {Project} from '../../../lib/project/project';
 import {Measurement} from '../../../lib/measurement/measurement';
 import {User} from '../../../lib/user/user';
-import {Panel, PanelGroup} from 'react-bootstrap';
-import {contains, without, sort} from 'ramda';
+import {sort} from 'ramda';
+import {MeasurementEntry} from './measurement-entry';
+import {PanelGroup} from 'react-bootstrap';
 
 interface Props
 {
@@ -11,58 +12,25 @@ interface Props
     project: Project;
     measurements: Measurement[];
 }
-interface State
+
+export class MeasurementList extends PureComponent<Props>
 {
-    expandedMeasurements: string[];
-}
-
-export class MeasurementList extends PureComponent<Props, State>
-{
-    constructor(props: Props)
-    {
-        super(props);
-
-        this.state = {
-            expandedMeasurements: []
-        };
-    }
-
     render()
     {
         const measurements = sort((m1: Measurement, m2: Measurement) =>
             m1.timestamp.isBefore(m2.timestamp) ? 1 : -1, this.props.measurements);
         return (
-            <div>
+            <PanelGroup id='measurement-list'>
                 {measurements.map(measurement =>
                     this.renderMeasurement(measurement))}
-            </div>
+            </PanelGroup>
         );
     }
 
     renderMeasurement = (measurement: Measurement): JSX.Element =>
     {
         return (
-            <div key={measurement.id}>
-                <span onClick={() => this.toggleExpand(measurement.id)}>
-                    Benchmark {measurement.benchmark} - {measurement.timestamp.format('DD. MM. YYYY HH:mm:ss')}
-                </span>
-                {contains(measurement.id, this.state.expandedMeasurements) &&
-                    <pre>
-                        {JSON.stringify({
-                            benchmark: measurement.benchmark,
-                            environment: measurement.environment,
-                            result: measurement.result
-                        }, null, 2)}
-                    </pre>}
-            </div>
+            <MeasurementEntry key={measurement.id} measurement={measurement} />
         );
-    }
-
-    toggleExpand = (id: string) =>
-    {
-        this.setState((state: State) => ({
-            expandedMeasurements: contains(id, state.expandedMeasurements) ?
-                without([id], state.expandedMeasurements) : state.expandedMeasurements.concat([id])
-        }));
     }
 }
