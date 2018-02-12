45a1818869c86788a639a693cdf83664f0797251
Jakub Beránek <berykubik@gmail.com>
2018-02-12 22:33:35 +0100

    ENH: optimize measurement list

--- dashboard/src/components/project/measurement-list/measurement-list.tsx
+++ dashboard/src/components/project/measurement-list/measurement-list.tsx
@@ -30,41 +30,31 @@ export class MeasurementList extends PureComponent<Props, State>
     render()
     {
         const measurements = sort((m1: Measurement, m2: Measurement) =>
-                m1.timestamp.isBefore(m2.timestamp) ? 1 : -1, this.props.measurements);
-
+            m1.timestamp.isBefore(m2.timestamp) ? 1 : -1, this.props.measurements);
         return (
-            <PanelGroup id='measurement-list'>
+            <div>
                 {measurements.map(measurement =>
                     this.renderMeasurement(measurement))}
-            </PanelGroup>
+            </div>
         );
     }
 
     renderMeasurement = (measurement: Measurement): JSX.Element =>
     {
         return (
-            <Panel key={measurement.id}
-                   expanded={contains(measurement.id, this.state.expandedMeasurements)}
-                   onToggle={() => this.toggleExpand(measurement.id)}>
-                <Panel.Heading>
-                    <Panel.Title toggle>
-                        Benchmark {measurement.benchmark} - {measurement.timestamp.format('DD. MM. YYYY HH:mm:ss')}
-                    </Panel.Title>
-                </Panel.Heading>
-                <Panel.Collapse>
-                    <Panel.Body>
-                        <div>
-                            <pre>
-                                {JSON.stringify({
-                                    benchmark: measurement.benchmark,
-                                    environment: measurement.environment,
-                                    result: measurement.result
-                                }, null, 2)}
-                            </pre>
-                        </div>
-                    </Panel.Body>
-                </Panel.Collapse>
-            </Panel>
+            <div key={measurement.id}>
+                <span onClick={() => this.toggleExpand(measurement.id)}>
+                    Benchmark {measurement.benchmark} - {measurement.timestamp.format('DD. MM. YYYY HH:mm:ss')}
+                </span>
+                {contains(measurement.id, this.state.expandedMeasurements) &&
+                    <pre>
+                        {JSON.stringify({
+                            benchmark: measurement.benchmark,
+                            environment: measurement.environment,
+                            result: measurement.result
+                        }, null, 2)}
+                    </pre>}
+            </div>
         );
     }
 
