99997a05afd12d2a0b270c0d2de3f224dd4c47d4
Jakub Beránek <berykubik@gmail.com>
2018-04-24 00:48:41 +0200

    FIX: API URL is now set correctly in production mode

--- dashboard/src/configuration.tsx
+++ dashboard/src/configuration.tsx
@@ -1,7 +1,7 @@
 function parseVariable(name: string, defaultValue: string = ''): string
 {
     const variable = window[name];
-    return variable !== undefined && variable !== `{{${name}}}` ? variable : defaultValue;
+    return (variable !== undefined && variable !== `{{${name}}}`) ? variable : defaultValue;
 }
 
 export const API_SERVER = parseVariable('API_SERVER', 'http://localhost:5000');
--- dashboard/src/server.tsx
+++ dashboard/src/server.tsx
@@ -7,10 +7,10 @@ import {promisify} from 'util';
 const app = express();
 const port = process.env.PORT || 3000;
 const webDir = process.env.WEB_PATH || path.join(__dirname, 'web');
-const apiHost = process.env.API_SERVER || 'http://localhost:5000';
+const apiServer = process.env.API_SERVER || 'http://localhost:5000';
 const urlPrefix = process.env.URL_PREFIX || '/';
 
-console.log(`Serving from ${webDir} on port ${port}, API address: ${apiHost}, URL prefix: ${urlPrefix}`);
+console.log(`Serving from ${webDir} on port ${port}, API address: ${apiServer}, URL prefix: ${urlPrefix}`);
 
 const readFileAsync = promisify(readFile);
 
@@ -18,7 +18,7 @@ async function readIndexFile(): Promise<string>
 {
     const html = await readFileAsync(path.join(webDir, 'index.html'));
     return html.toString()
-        .replace('{{API_HOST}}', apiHost)
+        .replace('{{API_SERVER}}', apiServer)
         .replace('{{URL_PREFIX}}', urlPrefix);
 }
 
