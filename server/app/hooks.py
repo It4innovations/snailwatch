7f39d71152845f1fd8508727c00c4dbadf61f389
Jakub Beránek <jakub.beranek@vsb.cz>
2018-09-07 11:00:37 +0200

    REF: move hooks to a separate file

---
+++ server/app/hooks.py
@@ -0,0 +1,90 @@
+import datetime
+from functools import reduce
+
+from flask import current_app as app, request
+
+from .auth import generate_token, hash_password
+from .db.project import ProjectRepo
+from .db.uploadtoken import UploadTokenRepo
+from .db.user import UserRepo
+from .db.view import ViewRepo
+from .util import get_dict_keys
+
+
+def before_insert_user(users):
+    for user in users:
+        user['password'] = hash_password(user['password'])
+    return users
+
+
+def before_insert_project(projects):
+    user_repo = UserRepo(app)
+    user = user_repo.get_user_from_request(request)
+
+    for project in projects:
+        project['writers'] = [user['_id']]
+        project['measurementKeys'] = []
+        project['repository'] = ''
+        project['commitKey'] = 'environment.commit'
+
+
+def after_insert_project(projects):
+    user_repo = UserRepo(app)
+    user = user_repo.get_user_from_request(request)
+    session_repo = UploadTokenRepo(app)
+
+    for project in projects:
+        session_repo.create_token(project, user, generate_token())
+
+
+def before_insert_measurement(measurements):
+    session_repo = UploadTokenRepo(app)
+    session = session_repo.get_token_from_request(request)
+
+    project_id = session['project']
+    project_repo = ProjectRepo(app)
+
+    info = {
+        'ip': request.remote_addr
+    }
+
+    benchmarks = {}
+    keys = set()
+    for measurement in measurements:
+        measurement['info'] = info
+        measurement['project'] = project_id
+        if 'timestamp' not in measurement:
+            measurement['timestamp'] = datetime.datetime.utcnow()
+
+        keys.update(get_dict_keys({
+            'benchmark': measurement['benchmark'],
+            'timestamp': measurement['timestamp'],
+            'environment': measurement['environment'],
+            'result': measurement['result'],
+        }))
+        benchmarks.setdefault(measurement['benchmark'], []).append(measurement)
+
+    project_repo.add_measurement_keys(project_id, list(keys))
+
+    view_repo = ViewRepo(app)
+    for (benchmark, measurements) in benchmarks.items():
+        views = list(view_repo.get_views_with_benchmark(benchmark))
+        if not views and benchmark:
+            y_axes = [set(m['result'].keys())
+                      for m in measurements if m['result']]
+            union = reduce(lambda a, b: a.union(b), y_axes, set())
+            y_axes = reduce(lambda a, b: a.intersection(b), y_axes, union)
+            y_axes = ['result.{}.value'.format(y) for y in y_axes]
+
+            view_repo.create_internal(project_id, benchmark, [{
+                'path': 'benchmark',
+                'operator': '==',
+                'value': benchmark
+            }], y_axes)
+
+
+def init_hooks(app):
+    app.on_insert_users += before_insert_user
+    app.on_insert_projects += before_insert_project
+    app.on_inserted_projects += after_insert_project
+    app.on_insert_measurements += before_insert_measurement
--- server/app/snailwatch.py
+++ server/app/snailwatch.py
@@ -4,6 +4,7 @@ from flask_cors import CORS
 from .auth import TokenAuthenticator
 from .configuration import get_server_port
 from .db.database import init_database
+from .hooks import init_hooks
 from .routes import setup_routes
 
 
@@ -13,5 +14,7 @@ def start():
 
     with app.app_context():
         init_database(app)
-    setup_routes(app)
+        init_hooks(app)
+        setup_routes(app)
+
     app.run(threaded=True, host='0.0.0.0', port=get_server_port())
