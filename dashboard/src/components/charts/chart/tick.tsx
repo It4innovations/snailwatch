c6a690f1516de6a6f06432724d46db2cfc910856
Jakub Beránek <jakub.beranek@vsb.cz>
2018-10-17 09:47:35 +0200

    ENH: increase X axis untrimmed length

--- dashboard/src/components/charts/chart/tick.tsx
+++ dashboard/src/components/charts/chart/tick.tsx
@@ -1,4 +1,3 @@
-import ellipsize from 'ellipsize';
 import React, {PureComponent} from 'react';
 
 interface Props
@@ -13,22 +12,38 @@ interface Props
         value: string;
     };
 }
+interface DefaultProps {
+    maxLength: number;
+}
 
-export class Tick extends PureComponent<Props>
+export class Tick extends PureComponent<Props & Partial<DefaultProps>>
 {
+    static defaultProps: DefaultProps = {
+        maxLength: 30
+    };
+
     render()
     {
         const {width, height, x, y, fill, textAnchor} = this.props;
         const value = this.props.payload.value;
 
+        const shortened = this.shorten(value);
+
         return (
             <g>
                 <text width={width} height={height} x={x} y={y} fill={fill}
                       textAnchor={textAnchor}>
-                    <tspan x={x} dy='0.71em'>{ellipsize(value, 10)}</tspan>
+                    <tspan x={x} dy='0.71em'>{shortened}</tspan>
                 </text>
                 <title>{value}</title>
             </g>
         );
+    }
+
+    shorten = (value: string): string =>
+    {
+        if (value.length < this.props.maxLength) return value;
+
+        return `${value.substr(0, this.props.maxLength)}…`;
     }
 }
